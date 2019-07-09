21db45e743219a848b2dcae8149742c5333d7a7c
farid <engfaridooo@gmail.com>
2019-07-09 06:36:33 -0700

    Initial Commit

---
+++ Resort Real Estate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Resort Real Estate
+//
+//  Created by Farido on 7/9/19.
+//  Copyright © 2019 Farido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
